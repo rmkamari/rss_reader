413652fe5ed042b7322d3b26f9179b4069bc3245
rmk <ma.fani@gitlab.rh>
2019-12-02 11:01:46 +0330

    mongo address update

--- mongodb.py
+++ mongodb.py
@@ -3,7 +3,12 @@ from pymongo import MongoClient
 
 def get_rss():
     try:
-        client = MongoClient('mongodb://localhost:27017/')
+        client = MongoClient("mongodb://rssMd:rssMd1081qaz"
+                             "mongo-rs-0-mongod.percona-server-mongodb.autoip.dcos.thisdcos.directory,"
+                             "mongo-rs-1-mongod.percona-server-mongodb.autoip.dcos.thisdcos.directory,"
+                             "mongo-rs-2-mongod.percona-server-mongodb.autoip.dcos.thisdcos.directory:27017"
+                             "/rssDb?replicaSet=rs")
+        # client = MongoClient('mongodb://localhost:27017/')
         db = client.rss_reader  # Connection to the database
         my_col = db['rss_link']
     except Exception as err:
